314db89db874ebe18c1295bcf8913ba71e658b79
thanan29 <thanancariappa2002@gmail.com>
2024-07-12 02:33:16 -0700

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,19 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!"
+        System.out.println("Hello, World!");
+
+        // Declare two integer variables
+        int num1 = 10;
+        int num2 = 20;
+
+        // Calculate the sum of num1 and num2
+        int sum = num1 + num2;
+
+        // Print the sum
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+
+        // Print a goodbye message
+        System.out.println("Goodbye!");
+    }
+}
